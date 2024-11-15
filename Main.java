6ae0901a301e2dda8aa931b53cf1e4555dfa9824
daail10 <daail15@my.yorku.ca>
2024-11-14 22:41:42 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package BrickBreaker;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+
+        frame.setBounds(10, 10, 700, 600);
+        frame.setTitle("Brick Breaker");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePlay);
+        frame.setVisible(true);
+    }
+}
